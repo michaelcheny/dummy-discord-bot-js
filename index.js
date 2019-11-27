5671048020e311b192386593b1c41a7489751ed9
Michael Chen <michaelchengaming@gmail.com>
2019-11-27 13:34:09 -0800

    seperate concerns, create fetch module for all api fetching work

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Implement fetch API to our Node app.
 const fetch = require("node-fetch");
-// const fetchmodule =  require('./fetchmodule')
+const fetchmodule =  require('./fetchmodule')
 
 // Require config
 const { prefix, token } = require('./config.json');
@@ -41,7 +41,7 @@ client.on('message', message => {
 
 // processes the requested command 
 function processCommand(message){
-    const args = message.content.slice(prefix.length).split(' ');
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (command == "help"){
@@ -49,11 +49,11 @@ function processCommand(message){
     } else if (command == "commands") {
         message.channel.send(commands);
     } else if (command == "meow"){
-        fetchCats().then(fact => message.channel.send(fact));
+        fetchmodule.randCatFact().then(fact => message.channel.send(fact));
     } else if (command == "woof"){
-        randomDogPhoto().then(attachment => message.channel.send(attachment));
-    } else if (primaryCommand == "purr"){
-        randomCatPhoto().then(attachment => message.channel.send(attachment));
+        fetchmodule.randDogPhoto().then(attachment => message.channel.send(attachment));
+    } else if (command == "purr"){
+        fetchmodule.randCatPhoto().then(attachment => message.channel.send(attachment));
     } else {
         message.channel.send("I don't know that command.");
     };
@@ -75,38 +75,3 @@ let commands = [
     "!purr - Random cat photo"
 ];
 
-// returns a random cat fact
-async function fetchCats(){
-    try {
-        let catRes = await fetch('https://cat-fact.herokuapp.com/facts');
-        let catJson = await catRes.json();
-        let randomCatFact = catJson.all[Math.floor(Math.random() * catJson.all.length)];
-        return randomCatFact.text;
-    } catch (error){
-        console.log(error.message);
-        return error.message;
-    };
-}; 
-
-// returns a random dog pic
-async function randomDogPhoto(){
-    try {
-        let response = await fetch('https://dog.ceo/api/breeds/image/random');
-        let json = await response.json();
-        return new Discord.Attachment(json.message);
-    } catch (error) {
-        return error.message;
-    };
-};
-
-// returns a random cat pic
-async function randomCatPhoto(){
-    try {
-        let response = await fetch('https://api.thecatapi.com/v1/images/search');
-        let json = await response.json();
-        // console.log(json[0])
-        return new Discord.Attachment(json[0].url);
-    } catch (error) {
-        return error.message;
-    };
-};
