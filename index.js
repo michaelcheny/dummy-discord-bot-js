f180f00d3c2cb0d71bc34f6a3b434a2f8b5325a5
Michael Chen <michaelchengaming@gmail.com>
2019-11-27 03:39:01 -0800

    refactor and add comments

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fetch = require("node-fetch");
 // const fetchmodule =  require('./fetchmodule')
 
 // Require config
-// const config = require('./config.json');
 const { prefix, token } = require('./config.json');
 
 // Requires the discord node modules
@@ -17,18 +16,16 @@ const botChannels = ["649170818974089226"];
 // Logs Ready as soon as bot is ready.
 client.once('ready', () => {
     console.log('Bot: ' + client.user.tag);
-    client.guilds.forEach(guild => {
-        console.log(guild.name);
-        guild.channels.forEach(channel => {
-            console.log(` -${channel.name} ${channel.type} ${channel.id}`)
-            // water collab bot-test: 649124226682585118
-            // empty bot-test: 649170818974089226
-        });
-    });
+    // client.guilds.forEach(guild => {
+    //     console.log(guild.name);
+    //     guild.channels.forEach(channel => {
+    //         console.log(` -${channel.name} ${channel.type} ${channel.id}`)
+    //     });
+    // });
     botChannels.forEach(channel => {
         let botChannel = client.channels.get(channel);
         // if (!!botChannel){
-            botChannel.send("oooWeeee");
+            botChannel.send("OOOooWeeee! Bot activated!");
         // };
     });
 });
@@ -38,64 +35,47 @@ client.login(token);
 
 // reply
 client.on('message', message => {
-	if ( !message.content.startsWith(prefix) || message.author === client.user){
-        return;
-    };
-    // if (message.content.startsWith("!")){
-        processCommand(message);
-    // };
+	if ( !message.content.startsWith(prefix) || message.author.bot ) return;
+    processCommand(message);
 });
 
+// processes the requested command 
 function processCommand(message){
-    let fullCommand = message.content.substr(1);
-    let splitCommand = fullCommand.split(" ");
-    let primaryCommand = splitCommand[0];
-    let args = splitCommand.splice(1);
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
 
-    if (primaryCommand == "help"){
+    if (command == "help"){
         helpCommand(args, message);
-    } else if (primaryCommand == "commands") {
+    } else if (command == "commands") {
         message.channel.send(commands);
-    } else if (primaryCommand == "meow"){
+    } else if (command == "meow"){
         fetchCats().then(fact => message.channel.send(fact));
-    } else if (primaryCommand == "woof"){
-        randomDogPhoto().then(msg => message.channel.send(msg));
+    } else if (command == "woof"){
+        randomDogPhoto().then(attachment => message.channel.send(attachment));
+    } else if (primaryCommand == "purr"){
+        randomCatPhoto().then(attachment => message.channel.send(attachment));
     } else {
         message.channel.send("I don't know that command.");
     };
 };
 
+// help command takes multiple args
 function helpCommand(args, message){
     if (args.length == 0){
-        message.channel.send("What? try `!help [topic]`")
+        message.channel.send("What? try `!help [topic]`");
     } else {
 
     };
 };
 
-
-// client.on('message', message => {
-//     switch (message.content){
-//         case `${prefix}commands`:
-//             message.channel.send(commands);
-//             break;
-//         case `${prefix}meow`:
-//             fetchCats().then(fact => message.channel.send(fact));
-//             break;
-//         case  `${prefix}woof`:
-//             randomDogPhoto().then(msg => message.channel.send(msg));
-//     }
-// });
-
-
+// commands
 let commands = [
     "!meow - Random cat fact", 
-    "!woof - Random Dog photo",
-    
+    "!woof - Random dog photo",
+    "!purr - Random cat photo"
 ];
 
-
-
+// returns a random cat fact
 async function fetchCats(){
     try {
         let catRes = await fetch('https://cat-fact.herokuapp.com/facts');
@@ -108,12 +88,25 @@ async function fetchCats(){
     };
 }; 
 
+// returns a random dog pic
 async function randomDogPhoto(){
     try {
         let response = await fetch('https://dog.ceo/api/breeds/image/random');
         let json = await response.json();
         return new Discord.Attachment(json.message);
     } catch (error) {
-        return error.message
+        return error.message;
+    };
+};
+
+// returns a random cat pic
+async function randomCatPhoto(){
+    try {
+        let response = await fetch('https://api.thecatapi.com/v1/images/search');
+        let json = await response.json();
+        // console.log(json[0])
+        return new Discord.Attachment(json[0].url);
+    } catch (error) {
+        return error.message;
     };
 };
