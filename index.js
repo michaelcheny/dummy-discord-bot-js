e860e32fa51b9e14807da8ab839183f02f03239c
Michael Chen <michaelchengaming@gmail.com>
2020-07-21 01:08:58 -0700

    use dotenv to store bot token and prefix

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Require config
-const { prefix, token } = require('./config.json');
+require("dotenv").config();
 
 // Requires the discord node modules
-const fs = require('fs');
-const Discord = require('discord.js');
+const fs = require("fs");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-const commandFiles = fs
-  .readdirSync('./commands')
-  .filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -16,42 +14,40 @@ for (const file of commandFiles) {
 }
 
 // bot channels
-const botChannels = ['649170818974089226'];
+const botChannels = ["649170818974089226"];
 // "649124226682585118" - water collab
 
 // Logs Ready as soon as bot is ready.
-client.once('ready', () => {
-  console.log('Bot: ' + client.user.tag);
+client.once("ready", () => {
+  console.log("Bot: " + client.user.tag);
   // client.guilds.forEach(guild => {
   //     console.log(guild.name);
   //     guild.channels.forEach(channel => {
   //         console.log(` -${channel.name} ${channel.type} ${channel.id}`)
   //     });
   // });
-  botChannels.forEach(channel => {
+  botChannels.forEach((channel) => {
     let botChannel = client.channels.get(channel);
-    botChannel.send('OOOooWeeee! Bot activated!');
+    botChannel.send("OOOooWeeee! Bot activated!");
   });
 });
 
 // Logs in with the secret token
-client.login(token);
+client.login(process.env.BOT_TOKEN);
 
 // reply
-client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", (message) => {
+  if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
   processCommand(message);
 });
 
 // processes the requested command
 function processCommand(message) {
-  const args = message.content.slice(prefix.length).split(/ +/);
+  const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
 
   if (!client.commands.has(commandName))
-    return message.reply(
-      'there was an error trying to execute that command! Try `!commands`'
-    );
+    return message.reply("there was an error trying to execute that command! Try `!commands`");
 
   const command = client.commands.get(commandName);
 
@@ -59,6 +55,6 @@ function processCommand(message) {
     command.execute(message, args);
   } catch (error) {
     console.error(error);
-    message.reply('there was an error trying to execute that command!');
+    message.reply("there was an error trying to execute that command!");
   }
 }
