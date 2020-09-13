0872c24829cca97e203b8ae8cff13dad36483de5
Michael Chen <michaelchengaming@gmail.com>
2020-09-12 23:56:46 -0700

    fix error where command message wasnt showing inline text

--- commands/commands.js
+++ commands/commands.js
@@ -11,6 +11,6 @@ let commands = [
   // '```!meow - Random cat fact.',
   // '!woof - Random dog photo.',
   // '!purr - Random cat photo.',
-  '!nbastats [First Name] [Last Name] - Stats for the player (updated every 10 mins).',
+  '```!nbastats [First Name] [Last Name] - Stats for the player (updated every 10 mins).',
   '!telljoke [category] - Tell a random joke.```',
 ];
